8f44b32e2d83d806a369b50f76ca0bc1fd143c9a
yqs <lotusyu@163.com>
2018-07-25 14:39:49 +0800

    加入测试代码

--- net/src/main/java/com/lotusyu/net/tcp/netty/client/NettyTcpClient.java
+++ net/src/main/java/com/lotusyu/net/tcp/netty/client/NettyTcpClient.java
@@ -6,7 +6,12 @@ package com.lotusyu.net.tcp.netty.client;
  **/
 
 import com.lotusyu.net.tcp.netty.NettyTcpBase;
+import com.lotusyu.net.tcp.netty.handlers.ChildChannelHandler;
+import com.lotusyu.net.tcp.netty.handlers.InputPrinthandler;
+import com.lotusyu.net.tcp.netty.server.NettyTcpServer;
 import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelOption;
@@ -15,6 +20,12 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static com.lotusyu.net.tcp.netty.handlers.ChildChannelHandler.newLengthFieldBasedFrameDecoder;
 
 /**
  * netty 实现的TCPClient 一个对象表示一个连接
@@ -63,8 +74,54 @@ public class NettyTcpClient extends NettyTcpBase {
     }
 
     public static void main(String[] args) throws Exception {
+        int port = 1234;
+        String host = "localhost";
+        int msgNum = 1000000;
+        if(args!=null && args.length>1){
+            port = Integer.parseInt(args[1]);
+            host = args[0];
+            if(args.length>2){
+                msgNum = Integer.parseInt(args[2]);
+            }
+        }
 
-        // Configure the client.
+        NettyTcpClient client = new NettyTcpClient(host,port);
+        CountDownLatch c = new CountDownLatch(1);
+        AtomicInteger msgCounter = new AtomicInteger();
+        int finalMsgNum = msgNum;
+        client.setChildHandler(new ChildChannelHandler((ch)->{
+            ch.pipeline().addLast(newLengthFieldBasedFrameDecoder()).addLast(
+                    new InputPrinthandler((ctx, msg) -> {
+                        ByteBuf m = (ByteBuf) msg;
+                        m.release();
+                        if(msgCounter.incrementAndGet()==finalMsgNum){
+                            System.out.println("completed");
+                            c.countDown();
+                        }
+                    })
+            );
+        }));
+        Channel connect = client.connect();
+        String msg = new Random().ints().limit(100).collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString();
+        byte[] bytes = msg.getBytes();
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < msgNum; i++) {
+            ByteBuf msgBuf = Unpooled.buffer(bytes.length+4).writeInt(bytes.length).writeBytes(bytes);
+            connect.write(msgBuf);
+            if(i%10000==0){
+                connect.flush();
+                while(!connect.isWritable()){
+                    System.out.println("sleep");
+                    Thread.sleep(100);
+                }
+            }
+        }
+        connect.flush();
+        c.await();
+        long end = System.currentTimeMillis();
+        long cost = end -start;
+        System.out.println("cost:"+cost+"\tqps:"+msgNum*1000L/cost);
 
     }
 }
